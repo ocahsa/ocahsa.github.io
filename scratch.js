0ffbfb038dd1b80c969450e35d95ff1ad3e0e759
Ismail Manjlai <83699039+ocahsa@users.noreply.github.com>
2021-08-02 08:19:08 -0400

    deleted fetch req

--- scratch.js
+++ scratch.js
@@ -1,5 +1,1 @@
 
-
-fetch("https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/GDHeXhAlQqscEyo_E6Jphv7Oiy0z8v88jxG4t9rj82b8Muk?api_key=RGAPI-bf1e6d69-7122-454b-bc39-9b2a04558f0b", {
-    mode: "no-cors"
-}).then((response) => response).then((responseBody) => console.log(responseBody));
