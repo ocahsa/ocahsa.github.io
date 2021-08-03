30a4637c757061624bc958bbdbd2812617f8943d
Ismail <inmanjlai@hotmail.com>
2021-08-02 20:04:19 -0400

    current website

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // do nothing for now.
+window.addEventListener(("DOMContentLoaded"), () => {
+    const openSeq = document.querySelector(".openSeq");
+    setTimeout(() => {
+        openSeq.innerHTML= ``;
+        console.log('Removed openSeq')
+    }, 9500);
+
+    const mainContent = document.querySelector(".mainContent");
+    setTimeout(() => {
+        mainContent.style.display="block";
+        console.log("Initialized main site")
+    }, 10000);
+})
